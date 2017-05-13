a3767b7723125b316d52856ce01f45be620916f0
Guilherme Okita <gui_okita@hotmail.com>
2017-05-14 08:03:36 +0900

    Atualizado algumas expressoes
    A fim de fazer a aritmética em 48bits, foi alterada algumas expressões

--- Sources/Interpret.c
+++ Sources/Interpret.c
@@ -1,21 +1,38 @@
 #include "../Headers/Interpret.h"
 #include <stdio.h>
+#include <stdint.h>
+
+typedef struct{
+    uint64_t field:48;
+}uint48_t;
 
 //-----------------OPERAÇÕES--------------------
 long long adicionar(long long a, long long b){
-    return a+b;
+    uint48_t ax, bx;
+    ax.field =a;
+    bx.field = b;
+    return ax.field+bx.field;
 }
 
 long long subtrair(long long a, long long b){
-    return a-b;
+    uint48_t ax, bx;
+    ax.field =a;
+    bx.field = b;
+    return ax.field-bx.field;
 }
 
 long long dividir(long long a, long long b){
+    uint48_t ax, bx;
+    ax.field =a;
+    bx.field = b;
     return a/b;
 }
 
 long long multiplicar(long long a, long long b){
-    return a*b;
+    uint48_t ax, bx;
+    ax.field =a;
+    bx.field = b;
+    return ax.field*bx.field;
 }
 
 long long numero(Nodo* a){
@@ -32,7 +49,12 @@ static Pilha* pilha;
 //Interpreta a expressao passada, transformando-a em notacao Polonesa Reversa e faz a avaliacao da pilha
 long long Interpret(char* expressao){
     pilha = New_Pilha();
-
+/*******************************************
+*TODO
+*   COLOCAR A AVALIACAO DA EXPRESSAO PARA RPN
+*   AQUI
+*
+********************************************/
     Pilha_Push(pilha, New_Nodo(5,VAL));
     Pilha_Push(pilha, New_Nodo(3,VAL));
     Pilha_Push(pilha, New_Nodo(5,ADD));
@@ -43,16 +65,19 @@ long long Interpret(char* expressao){
     Pilha_Push(pilha, New_Nodo(0,SUB));
     Pilha_Push(pilha, New_Nodo(0,DIV));
 
+
     long long value = Evaluate(Pilha_Pop(pilha));
     free(pilha);
     return value;
 }
-
+int i = 0;
 //Faz a avaliacao do nodo e retorna o valor da avaliacao
 long long Evaluate(Nodo* a){
     //Se o nodo for do tipo operacao é avaliado o lado direito e esquerdo
     //e entao é retornado o resultado da operacao
+    i++;
     if(Nodo_Get_Type(a)){
+
         Nodo* aux = Pilha_Pop(pilha);
         //Avalia o lado direito
         long long right = Evaluate(aux);
@@ -62,8 +87,10 @@ long long Evaluate(Nodo* a){
         long long left = Evaluate(aux);
         free(aux);
         //Executa a operação
-        long long res = list_of_operations[Nodo_Get_Type(a) - 1](left , right);
+        long long res = list_of_operations[Nodo_Get_Type(a) - 1](left, right);
         free(a);
+        //Checa se o numero a retornar deve ser negativo
+        res |= ((res>>47)&1)?0xffff000000000000:0x0;
         return res;
     }else{
         //Se o tipo for Valor então retorna o valor.
