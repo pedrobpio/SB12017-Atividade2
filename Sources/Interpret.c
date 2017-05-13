960564c3ca99722fd208a508fa2d43cb03ef78fd
Guilherme Okita <gui_okita@hotmail.com>
2017-05-14 06:34:50 +0900

    Add files via upload

--- Sources/Interpret.c
+++ Sources/Interpret.c
@@ -1,4 +1,5 @@
 #include "../Headers/Interpret.h"
+#include <stdio.h>
 
 long long adicionar(long long a, long long b){
     return a+b;
@@ -22,13 +23,43 @@ long long numero(Nodo* a){
 
 long long (*list_of_operations[4]) (long long, long long) = {adicionar, subtrair, multiplicar, dividir};
 
+static Pilha* pilha;
+
 long long Interpret(char* expressao){
-    Pilha* pilha = New_Pilha();
+    pilha = New_Pilha();
+    Pilha_Push(pilha, New_Nodo(3,VAL));
+    Pilha_Push(pilha, New_Nodo(11,VAL));
+    Pilha_Push(pilha, New_Nodo(5,VAL));
+    Pilha_Push(pilha, New_Nodo(0,ADD));
+    Pilha_Push(pilha, New_Nodo(0,SUB));
+    /*Pilha_Push(pilha, New_Nodo(4,VAL));
+    Pilha_Push(pilha, New_Nodo(6,VAL));
+    Pilha_Push(pilha, New_Nodo(0,SUB));
+    Pilha_Push(pilha, New_Nodo(0,DIV));*/
 
+    long long value = Evaluate(Pilha_Pop(pilha));
     free(pilha);
+    return value;
 }
+char* operation = {"+-*/"};
+long long Evaluate(Nodo* a){
+    //printf("%d\n", Pilha_Get_Size(pilha));
+    if(Nodo_Get_Type(a)){
 
-long long Evaluate(long long (*operation)(long long, long long), Nodo* a, Nodo* b){
+        Nodo* aux = Pilha_Pop(pilha);
 
+        long long right = Evaluate(aux);
+        free(aux);
+
+        aux = Pilha_Pop(pilha);
+        long long left = Evaluate(aux);
+        free(aux);
+
+        long long res = list_of_operations[Nodo_Get_Type(a) - 1](left , right);
+        free(a);
+
+        return res;
+    }else{
+        return Nodo_Get_Value(a);
+    }
 }
-
